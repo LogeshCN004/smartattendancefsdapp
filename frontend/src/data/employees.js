6d46e9641468321dc896e1cfa106e731796f3607
C N Logesh Kumar <logeshkumarcn@gmail.com>
2026-03-10 12:10:37 +0530

    Delete frontend directory

--- frontend/src/data/employees.js
+++
@@ -1,3 +0,0 @@
-export const fallbackEmployees = [
-  { id: 1, name: "Fallback User", department: "Engineering", attendancePercentage: 80, productivityScore: 70, workingHours: 160, lateDays: 1 }
-];
